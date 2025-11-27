503f25228b1fc309ac49effb6e62b501656338e9
Samuel Ayer <ayersamuel07@gmail.com>
2025-11-27 15:54:02 +0000

    Initial commit

---
+++ cleanCSV.py
@@ -0,0 +1,192 @@
+import sys
+import os
+from pathlib import Path
+import pandas as pd
+import time
+import numpy as np
+import logging
+
+# exit program if no argument or more than one is given
+if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
+        print('You did not add a csv file when running this program.')
+    else:
+        print(f'You entered {len(sys.argv) - 1} arguments.')
+    print('This program only accepts one csv file as argument as follows: python3 cleanCSV.py [path/to/your/data.csv]')
+    sys.exit()
+
+# get filepath
+filepath = Path(sys.argv[1])
+
+# convert all filepaths to absolute
+abs_filepath = Path(filepath).resolve()
+
+# set up logging
+logger_name = str(abs_filepath)
+logger = logging.getLogger(logger_name)
+logger.setLevel(logging.DEBUG)
+
+# file for logging
+log_folder = Path('./logs')
+if log_folder.exists():
+    log_filepath = log_folder.joinpath('cleanCSV.logs')
+else:
+    Path.mkdir('./logs')
+    log_filepath = log_folder.joinpath('cleanCSV.logs')
+
+# create handler to write logs to file
+file_handler = logging.FileHandler(log_filepath, mode="a", encoding='utf-8')
+# set up file formatter
+file_Formatter = logging.Formatter("%(asctime)s - %(levelname)s - [%(name)s] %(message)s")
+file_handler.setFormatter(file_Formatter)
+
+# create handler to write logs to console
+console_handler = logging.StreamHandler()
+# set up console formatter
+console_formatter = logging.Formatter("%(message)s")
+# add formatter to console handler
+console_handler.setFormatter(console_formatter)
+
+# add handlers to logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+# check if argument supplied is a csv
+if filepath.suffix != '.csv':
+    logger.error('The argument supplied is not a csv file.')
+    sys.exit()
+
+# load csv into pandas dataframe
+try:
+    print(f'Loading file from {abs_filepath} ...')
+    df = pd.read_csv(rf'{abs_filepath}')
+    time.sleep(0.5)
+    logger.info('Loaded file')
+except Exception as e:
+    logger.error(f'An error occured: {e}')
+    sys.exit()
+
+# check if csv file is empty (only headers)
+if len(df) == 0:
+    logger.error('The file has only columns but no rows.')
+    sys.exit()
+
+
+# data profiling
+print('\n')
+print('Data Profile')
+
+# get number of columns and rows
+cols,rows = df.shape
+logger.info(f'The data has {cols} columns and {rows} rows.')
+
+# check for duplicates
+duplicateCount = df.duplicated().sum()
+logger.info(f'The data has {duplicateCount} duplicates.')
+
+# check for null
+# nullDf = df.isnull().sum()#.reset_index()
+# nullDf = pd.DataFrame(nullDf.reset_index()).rename(columns={'index':'columns', 0:'nulls'})
+# onlyNulls = nullDf[nullDf['nulls'] > 0]
+nullColumns = df.columns[df.isnull().any()].to_list()
+if len(nullColumns) == 0:
+    logger.info('There are no nulls.')
+else:
+    logger.warning(f'The following columns have null records: {nullColumns}')
+    
+
+# standardize column headers
+def standardize_headers(headers):
+    standardized_headers = {}
+    for header in headers:
+        standardized = header.strip().lower().replace(' ', '_')
+        standardized_headers[header] = standardized
+    return standardized_headers
+
+print('\n')
+print('Standardizing column headers...')
+print('The original column headers are:')
+print(list(df.columns))
+
+logger.info('Standardized column headers')
+new_headers = standardize_headers(df.columns)
+print(list(new_headers.values()))
+
+# rename column headers
+df.rename(columns=new_headers, inplace=True)
+
+# dropping duplicates, if they exist
+if duplicateCount > 0:
+    print(f'Removing {duplicateCount} duplicates')
+    keeps = None
+    duplResp = 0
+    while True:
+        if keeps in ['f', 'first', 'l', 'last']:
+            break
+        elif duplResp < 3  and keeps not in ['f', 'first', 'l', 'last']:
+            keeps = input("For each duplicate, do you want to keep the (f)irst or the (last): ")
+            keeps = keeps.lower()
+            duplResp += 1
+        elif duplResp >= 3:
+            logger.error('Wrong response entered 3 times.')
+            print('Please restart the program.')
+            sys.exit()
+
+    before_drop = len(df)
+    if keeps in ['l', 'last']:
+        df.drop_duplicates(keep='last', inplace=True)
+    else:
+        df.drop_duplicates(inplace=True)
+    afterdrop = before_drop - len(df)
+    logger.info(f'{afterdrop} duplicates have been dropped.')
+
+# data types
+shouldBeNumeric = []
+for column in df.columns:
+    if column not in df.select_dtypes(include=np.number).columns.to_list():
+        try:
+            df[column] = pd.to_numeric(df[column]) # df[column].astype('int')
+            logger.info(f'{column} has been converted to an integer.')
+        except ValueError:
+            continue
+
+def checkOutliers(df, column):
+    firstQuartile, thirdQuartile =  np.percentile(df[column], [25,75])
+    iqr = thirdQuartile - firstQuartile
+    lowerBoundary = firstQuartile - (1.5 * iqr)
+    upperBoundary = thirdQuartile + (1.5 * iqr)
+    # check for outliers
+    outliers = df[(df[column] < lowerBoundary) | (df[column] > upperBoundary)]
+    outlierCount = len(outliers)
+    if outlierCount > 0:
+        return True
+    else:
+        return False
+    
+# dropping or imputing nulls
+# get numeric columns
+numericColumns = df.select_dtypes(include= ['int64', 'float64']).columns.to_list()
+
+for column in numericColumns:
+    # check for nulls
+    nullCount = df[column].isnull().sum()
+    dfLen = len(df)
+    if nullCount == 0:
+        continue
+    nullPercentage = round((nullCount/dfLen) * 100,2)
+    logger.info(f'There are {nullCount} in the {column} column.')
+    print(f'And these mnake up {nullPercentage} of the values in that column.')
+    nullCmd = input('Would you like to (i)mpute these values or (d)elete them: ')
+    if nullCmd.lower() == 'd':
+        continue
+    time.sleep(0.5)
+    print('Checking for outliers...')
+    time.sleep(0.5)
+    outlierStatus = checkOutliers(df,column)
+    if outlierStatus:
+        imputer = round(np.median(df[column]), 2)
+        logger.info(f'Since there are outliers in the data, nulls will be imputed with the median.')
+    else:
+        imputer = round(np.mean(df[column]), 2)
+        logger.info('There are no outliers in the data, so nulls will be imputed with the mean')
+    df[column] = df[column].fillna(imputer)
